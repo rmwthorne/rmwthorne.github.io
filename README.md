a7d7f4e096a19c88f96aae9a932f043586dfee9b
Ross Thorne <rthorne@c02yq8mblvdc.connect>
2020-05-23 13:55:15 +0100

    Revert "Updates"
    This reverts commit b8ea2f708c5667d71c8116a3fd2e79de73651a3e.

---
+++ README.md
@@ -0,0 +1,33 @@
+<h1 align="center">
+  Portfolio Website
+</h1>
+
+Portfolio website, built on [LekovicMilos/gatsby-starter-portfolio](https://www.gatsbyjs.org/starters/LekovicMilos/gatsby-starter-portfolio/) with many thanks.
+
+## Setup
+
+1.  **Install dependencies**
+
+    ```sh
+    nvm use 10
+    npm install gatsby-cli
+    ```
+
+2.  **Build**
+
+    Navigate into your your portfolio's directory and start it up.
+
+    ```sh
+    gatsby develop
+    ```
+
+    Your site is now running at `http://localhost:8000`!
+
+3.  **Deploy**
+
+    After applying your changes, run this command to deploy it.
+
+    ```sh
+    gatsby build
+    gatsby serve
+    ```
---
+++ portfolio/src/pages/about-me.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+import * as Mixins from '../Mixins';
+import * as t from '../Typography';
+import Layout, { Content } from '../components/Layout';
+import HireMePopup from '../components/HireMePopup.js';
+import { media } from '../MediaQueries';
+import Img from 'gatsby-image';
+import { graphql } from 'gatsby';
+
+const AboveFold = styled.div`
+  ${Mixins.aboveFoldMixin}
+  padding-bottom: 100px;
+
+  ${t.H1} {
+    margin-bottom: 25px;
+  }
+  ${t.H4} {
+    line-height: 1.14;
+  }
+  ${media.tablet`background-position: center top 0px;`};
+`;
+
+const AboutMeWrapper = styled.div`
+  ${Mixins.wrapper}
+  .m-b-60 {
+    margin-bottom: 60px;
+  }
+  ${t.LargeP} {
+    margin-bottom: 28px;
+  }
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    min-height: 540px;
+    ${media.phone`min-height: 620px;`};
+  }
+  .gatsby-image {
+    ${media.tablet`text-align: center;`}
+    div {
+      padding: 0;
+    }
+    picture img {
+      max-width: 85%;
+      position: relative;
+      ${media.tablet`max-width: 80%;`}
+    }
+  }
+  .avatar {
+    max-width: 400px;
+    width: 80%;
+    margin: 0 auto 100px auto;
+    border-radius: 50%;
+    display: block;
+    ${media.tablet`max-width: 70%;`}
+  }
+`;
+
+class AboutMe extends React.Component {
+  state = {
+    openHireMePopup: false
+  };
+
+  handleRequestDemoClose = () => {
+    this.setState({
+      openHireMePopup: false
+    });
+  };
+
+  openContactPopup = () => {
+    this.setState({
+      openHireMePopup: true
+    });
+  };
+
+  render() {
+    const { openHireMePopup } = this.state;
+    const { data } = this.props;
+    return (
+      <AboutMeWrapper>
+        <Layout theme="white" openContactPopup={this.openContactPopup}>
+          <AboveFold>
+            <t.H1 green align="center">
+              Ross Thorne, PhD
+            </t.H1>
+            <t.LargeP align="center" max70>
+              Computational Biologist with a demonstrated history of working in the biotechnology industry. Skilled in
+              bioinformatics, data analysis, and data visualisation. Proven to be a flexible and fast learner, and
+              highly capable to work well within a team. Strong sense of visual design for data presentation and clarity
+              of written communication. Experienced research scientist with a Doctor of Philosophy (PhD), focused in
+              Clinical Laboratory Science from University of Oxford.
+            </t.LargeP>
+          </AboveFold>
+          <Content>
+            <Img fluid={data.avatarAbout.childImageSharp.fluid} alt="Name Surname" className="avatar" />
+          </Content>
+        </Layout>
+        <HireMePopup open={openHireMePopup} handleClose={this.handleRequestDemoClose} />
+      </AboutMeWrapper>
+    );
+  }
+}
+
+export default AboutMe;
+
+export const pageQuery = graphql`
+  query {
+    avatarAbout: file(relativePath: { eq: "avatar.jpg" }) {
+      ...fluidImage
+    }
+  }
+`;
